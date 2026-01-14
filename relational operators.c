7b37dc0445961d003526d5c6f94fbeb3467d4b99
Nidhi21087 <nidhipotle2007@gmail.com>
2026-01-14 22:06:11 +0530

    Add files via upload

---
+++ relational operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int a,b;
+	// correct input prompt
+	printf("Enter two numbers: ");
+	scanf("%d %d", &a, &b);
+	//relation operators with exact format
+	printf("%d < %d: %s\n", a, b, (a < b) ? "True" : "False");
+	printf("%d <= %d: %s\n", a, b, (a <= b) ? "True" : "False");
+	printf("%d > %d: %s\n", a, b, (a > b) ? "True" : "False");
+	printf("%d >= %d: %s\n", a, b, (a >= b) ? "True" : "False");
+	printf("%d == %d: %s\n", a, b, (a == b) ? "True" : "False");
+	printf("%d != %d: %s\n", a, b, (a != b) ? "True" : "False");
+	return 0;
+}
